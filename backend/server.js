dc725fab736f57f852b11fdafbd7d0549f32899d
Kiruba <kirubagnanaraj6@gmail.com>
2026-07-01 12:25:13 +0530

    fix error

--- backend/server.js
+++ backend/server.js
@@ -1,57 +1,3 @@
-// require("dotenv").config();
-
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-
-// const app = express();
-
-// /* ===========================
-//    Middleware
-// =========================== */
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// /* ===========================
-//    MongoDB Connection
-// =========================== */
-
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => {
-//     console.log("MongoDB Connected");
-//   })
-//   .catch((err) => {
-//     console.log("MongoDB Connection Failed", err);
-//   });
-
-// /* ===========================
-//    Routes
-// =========================== */
-
-// const resumeRoutes = require("./routes/resumeRoutes");
-
-// app.use("/api/resume", resumeRoutes);
-
-// /* ===========================
-//    Test Route
-// =========================== */
-
-// app.get("/", (req, res) => {
-//   res.send("SkillScan AI Backend Running");
-// });
-
-// /* ===========================
-//    Server Start
-// =========================== */
-
-const PORT = 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
 
 
 require("dotenv").config();
@@ -59,22 +5,22 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-console.log("URI:", process.env.MONGO_URI);
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// ✅ FIXED HERE
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
+// Routes
 const resumeRoutes = require("./routes/resumeRoutes");
 app.use("/api/resume", resumeRoutes);
 
-const PORT = process.env.PORT || 3000;
+// PORT (ONLY ONE)
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
